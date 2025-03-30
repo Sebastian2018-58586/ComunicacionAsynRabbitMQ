362d13f93356f6f68b175b5f863b978bb839febb
Sebas <joan.saavedra@correounivalle.edu.co>
2025-03-29 20:36:33 -0500

    AsynRabbitMQ codelab

---
+++ pedido-servicee/src/main/java/co/edu/univalle/pedidoservicee/PedidoServiceeApplication.java
@@ -0,0 +1,14 @@
+package co.edu.univalle.pedidoservicee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoServiceeApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PedidoServiceeApplication.class, args);
+    }
+
+}
---
+++ pedido-servicee/src/test/java/co/edu/univalle/pedidoservicee/PedidoServiceeApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.univalle.pedidoservicee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoServiceeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
